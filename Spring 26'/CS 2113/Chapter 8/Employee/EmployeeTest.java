00e4d7750149913cd3bd5341b743aab8509a066c
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2025-12-26 12:54:32 -0600

    Add initial EmployeeTest class with sample Employee instances

---
+++ Spring 26'/CS 2113/Chapter 8/Employee/EmployeeTest.java
@@ -0,0 +1,7 @@
+public class EmployeeTest{
+    public static void main(String[] args){
+        Employee e1 = new Employee("Durga","Yerukala");
+        Employee e2 = new Employee("Jhansi","Yerukala");
+        
+    }
+}
