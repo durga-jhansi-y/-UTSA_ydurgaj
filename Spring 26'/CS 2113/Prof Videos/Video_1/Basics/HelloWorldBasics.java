6440a0a9c291157b0dddc16ea6e0bcfd37ea1e3a
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2026-01-10 16:06:47 -0600

    Add variable initialization in HelloWorldBasics main method

--- Spring 26'/CS 2113/Prof Videos/Video_1/Basics/HelloWorldBasics.java
+++ Spring 26'/CS 2113/Prof Videos/Video_1/Basics/HelloWorldBasics.java
@@ -2,6 +2,9 @@ package Basics;
 
 public class HelloWorldBasics{
     public static void main(String[] args){
+        // variable 
+        int num = 5;
+        String greeting = "Hello World!";
         
     }
 }
