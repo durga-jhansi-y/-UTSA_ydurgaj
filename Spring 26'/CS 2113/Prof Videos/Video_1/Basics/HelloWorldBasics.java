28e08c6008e7b8699025bc5ec35d3f33fc82b078
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2026-01-10 16:59:42 -0600

    Refactor HelloWorldBasics to improve structure and readability

--- Spring 26'/CS 2113/Prof Videos/Video_1/Basics/HelloWorldBasics.java
+++ Spring 26'/CS 2113/Prof Videos/Video_1/Basics/HelloWorldBasics.java
@@ -1,10 +1,32 @@
 package Basics;
 
 public class HelloWorldBasics{
+
+    
     public static void main(String[] args){
         // variable 
-        int num = 5;
+        // int num = 5;
         String greeting = "Hello World!";
+
+        int x = 0;
+        int y = 15;
+
+        if (x < y) {
+            System.out.println(greeting);
+        }else if (x > y) {
+            System.out.println("x is greater than y");
+        } else {
+            System.out.println("x is equal to y");
+        }
+        int i = 0;
+        while(i<10){
+            System.out.println("i is: " + i);
+            i++;
+        }
+
+        for ( i = 0 ; i <10 ; i++){
+            System.out.println("for loop i is: " + i);
+        }
         
     }
 }
