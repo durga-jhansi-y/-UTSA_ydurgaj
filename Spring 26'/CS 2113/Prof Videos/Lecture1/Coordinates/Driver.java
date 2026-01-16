eb19cec0ceb533b6a23f7bb98e6162cd5cc89b45
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2026-01-16 17:28:15 -0600

    Add Point and Driver classes for coordinate representation

---
+++ Spring 26'/CS 2113/Prof Videos/Lecture1/Coordinates/Driver.java
@@ -0,0 +1,10 @@
+package Coordinates;
+
+public class Driver {
+
+    public static void main(String[] args){
+        Point p = new Point();
+        System.out.println("Point p is at: " + p.toString());
+    }
+    
+}
---
+++ Spring 26'/CS 2113/Prof Videos/Lecture1/Coordinates/Point.java
@@ -0,0 +1,11 @@
+package Coordinates;
+
+public class Point {
+    int x = 0;
+    int y = 0;
+
+    public String toString(){
+        return "("+x+", "+y+")";
+    }
+    
+}
