6e782a1a47a96ae107558d21f9692adbdbf3c961
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2026-02-04 10:30:39 -0600

    Refactor code structure for improved readability and maintainability

---
+++ Spring 26'/CS 2713/CLASSNOTES/Intro_to_C/hello.c
@@ -0,0 +1,13 @@
+#include <stdio.h> // java's import ->header files : just like c 
+
+int main(int argc , char * argv[]) {
+  printf("Hello, World!\n"); // printf doiesnt end line so \n should be used  
+  return 0;
+}
+
+// import java.util.*; -> doesnt run in C ; imports the necessary libraries , classes and methods 
+// public class hello{ 
+//     public static void main(String[] args){
+//         System.out.println("Hello");
+//     }
+// }
