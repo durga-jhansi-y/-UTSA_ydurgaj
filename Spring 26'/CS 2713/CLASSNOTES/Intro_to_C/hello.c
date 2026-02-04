49d4abe419ed456f28546af17f177885ca02ad98
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2026-02-04 10:43:54 -0600

    Update hello.c and README_Prog_C for clarity and completeness

--- Spring 26'/CS 2713/CLASSNOTES/Intro_to_C/hello.c
+++ Spring 26'/CS 2713/CLASSNOTES/Intro_to_C/hello.c
@@ -1,8 +1,8 @@
-#include <stdio.h> // java's import ->header files : just like c 
+#include <stdio.h> // java's import ->header files : just like c with different extention .h (standard input output header file)
 
-int main(int argc , char * argv[]) {
-  printf("Hello, World!\n"); // printf doiesnt end line so \n should be used  
-  return 0;
+int main(int argc , char * argv[]) {// main function -> starting point of program execution 
+  printf("Hello, World!\n"); // printf doesnt end line so \n should be used  
+  return 0; // return type of main function is int so return 0 indicates successful execution 
 }
 
 // import java.util.*; -> doesnt run in C ; imports the necessary libraries , classes and methods 
