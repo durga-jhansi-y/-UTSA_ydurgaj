c283515a4289c637e0b9e2f9c1d9bbee63ac3d6b
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2026-02-28 08:22:32 -0600

    Implement random number generation within a specified range

--- Spring 26'/CS 2713/CLASSNOTES/Data/rands.c
+++ Spring 26'/CS 2713/CLASSNOTES/Data/rands.c
@@ -17,3 +17,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+int main(int argc , char* argv[]){
+    int L = -20;
+    int U = 20;
+    int N = U-L+1;
+
+    srand(time(NULL));
+
+    printf("Random number between %d and %d is %d\n", L, U, rand()%N +L);
+
+    for (int i =0 ; i<N ; i++){
+        int r = rand()%N+L;
+        printf("%d. %d\n",i+1,r);
+    }
+    return 0;
+
+}
