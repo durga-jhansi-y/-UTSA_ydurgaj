676eee579d81c9ab3a857bc22892da0d3c61cf06
Durga Jhansi Yerukala <ydurgaj@gmail.com>
2026-02-28 16:12:17 -0600

    Add BinarySortish and LinearS implementations with sorting and search functionality

---
+++ Spring 26'/CS 2123/Week2/BinarySortish.java
@@ -0,0 +1,23 @@
+public class BinarySortish {
+    public static void BinarySort(int[] arr){
+        int n = arr.length;
+        for (int i = 0 ; i<n-1 ; i++){
+            for (int j = 0 ; j<n-i-1 ; j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j] ; 
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    System.out.println("Swapping " + arr[j] + " and " + arr[j+1]);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        int[] arr = {1,8,99,5,83,4,6,3,-4};
+        BinarySort(arr);
+        for (int i = 0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ Spring 26'/CS 2123/Week2/LinearS.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class LinearS {
+    public static void main(String[] args){
+        int[] arr = {1,2,5,99,0,4,11,8, 5,5};
+        System.out.println("5 is at positions : " + LinearSear(arr,5));
+    }
+
+    public static int LinearSear(int[] arr, int k){
+        int count = 0;
+        int[] arr1 = new int[arr.length];
+        for (int i = 0 ; i<arr.length; i++){
+            if(arr[i] == k){
+                count++;
+                arr1[count-1] = i;
+            }
+        }
+        if(count == 0){
+            return -1;
+        }
+        else{
+            for (int i = 0; i<count; i++){
+                System.out.println("Element found at position: " + arr1[i]);
+            }
+        }
+        return count;
+    }
+    
+}
